8b9c56ef487407462a80ebbaef5337aa1c3b5ba0
Manuel Sousa <manuelsousamvs@gmail.com>
2016-10-02 17:55:46 +0100

    parte1 branch init

---
+++ src/contas.c
@@ -0,0 +1,52 @@
+#include "contas.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define atrasar() sleep(ATRASO)
+		     
+int contasSaldos[NUM_CONTAS];
+
+
+int contaExiste(int idConta) {
+  return (idConta > 0 && idConta <= NUM_CONTAS);
+}
+
+void inicializarContas() {
+  int i;
+  for (i=0; i<NUM_CONTAS; i++)
+    contasSaldos[i] = 0;
+}
+
+int debitar(int idConta, int valor) {
+  atrasar();
+  if (!contaExiste(idConta))
+    return -1;
+  if (contasSaldos[idConta - 1] < valor)
+    return -1;
+  atrasar();
+  contasSaldos[idConta - 1] -= valor;
+  return 0;
+}
+
+int creditar(int idConta, int valor) {
+  atrasar();
+  if (!contaExiste(idConta))
+    return -1;
+  contasSaldos[idConta - 1] += valor;
+  return 0;
+}
+
+int lerSaldo(int idConta) {
+  atrasar();
+  if (!contaExiste(idConta))
+    return -1;
+  return contasSaldos[idConta - 1];
+}
+
+
+void simular(int numAnos) {
+
+  /* POR COMPLETAR */
+      
+}
